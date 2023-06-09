ceca68d7bcc3564948805267910fbf39919d95f1
anshul313 <anshulsharma.sharma997@gmail.com>
2023-06-09 09:56:15 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const port = 3002;
+
+const app = express();
+
+const db = require("./config/mongoose");
+
+const mongoose = require("mongoose");
+const passport = require("passport");
+const passportJWT = require("./config/passport_jwt_strategy");
+
+//Middlewares
+app.use(express.urlencoded());
+
+app.use("/", require("./routes"));
+
+//listening to server
+
+app.listen(port, function (err) {
+  if (err) {
+    console.log("error in running the server");
+    return;
+  }
+  console.log("server is up and running on port:", port);
+  return;
+});
